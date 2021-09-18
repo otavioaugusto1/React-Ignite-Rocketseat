13f449320655c3b669356d3c4efe9728a378d3fc
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-09-18 13:18:45 -0300

    Chapter 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './styles/global.scss'
-
-export function App(){
+import { RepositoryList } from './components/RepositoryList'
+export function App() {
     return (
-        <h1>Hello World</h1>
-        )
+        <RepositoryList />
+    )
 }
