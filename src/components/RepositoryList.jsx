70ec67f63f8f0506a395caa12d5adde3992f3df4
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-09-19 20:45:41 -0300

    Chapter 1

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -6,22 +6,26 @@ import {useState, useEffect} from 'react';
 // https://api.github.com/orgs/rocketseat/repos
 
 
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://github.com/unform/unform'
-}
+
 
 export function RepositoryList() {
-    const [repositories, useRepositories] = useState([])
+    const [repositories, setRepositories] = useState([])
+    
+    useEffect(() => {
+        fetch("https://api.github.com/orgs/rocketseat/repos")
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, [])
+    
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
             <ul>
-                <ReposityItem repository = {repository}/>
-                <ReposityItem repository = {repository}/>
-                <ReposityItem repository = {repository}/>
-                <ReposityItem repository = {repository}/>
+                {repositories.map(repository => {
+                    return(
+                        <ReposityItem key = {repository.name} repository = {repository}/>
+                    )
+                })}
             </ul>
         </section>
     )
